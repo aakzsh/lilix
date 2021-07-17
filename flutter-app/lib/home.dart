68b596c872134b84cc1ceddcb17fd277bb871ae5
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-07-17 21:08:55 +0530

    got pedometer working

--- flutter-app/lib/home.dart
+++ flutter-app/lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lilix/dashboard.dart';
 import 'package:lilix/encrypt.dart';
+import 'package:lilix/pedometer.dart';
 import 'main.dart';
 
 class Home extends StatefulWidget {
--- flutter-app/lib/pedometer.dart
+++ flutter-app/lib/pedometer.dart
@@ -1,160 +1,118 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
 
-// import 'package:jiffy/jiffy.dart';
-// import 'package:pedometer/pedometer.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:pedometer/pedometer.dart';
 
-// class DailyStepsPage extends StatefulWidget {
-//   @override
-//   _DailyStepsPageState createState() => _DailyStepsPageState();
-// }
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
 
-// class _DailyStepsPageState extends State<DailyStepsPage> {
-//   Pedometer _pedometer;
-//   StreamSubscription<int> _subscription;
-//   Box<int> stepsBox = Hive.box('steps');
-//   int todaySteps;
+class lol extends StatefulWidget {
+  @override
+  _lolState createState() => _lolState();
+}
 
-//   final Color carbonBlack = Color(0xff1a1a1a);
+class _lolState extends State<lol> {
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     startListening();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: carbonBlack,
-//       appBar: AppBar(
-//         title: Text(
-//           "Daily Steps Tracker",
-//           style: GoogleFonts.darkerGrotesque(fontSize: 40),
-//         ),
-//         centerTitle: true,
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Spacer(),
-//             Card(
-//               color: Colors.black87.withOpacity(0.7),
-//               elevation: 3,
-//               shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(10)),
-//               child: Container(
-//                 margin: const EdgeInsets.only(
-//                   top: 10,
-//                   bottom: 30,
-//                   right: 20,
-//                   left: 20,
-//                 ),
-//                 child: Column(
-//                   children: <Widget>[
-//                     gradientShaderMask(
-//                       child: Text(
-//                         todaySteps?.toString() ?? '0',
-//                         style: GoogleFonts.darkerGrotesque(
-//                           fontSize: 80,
-//                           color: Colors.white,
-//                           fontWeight: FontWeight.w900,
-//                         ),
-//                       ),
-//                     ),
-//                     Text(
-//                       "Steps Today",
-//                       style: TextStyle(
-//                         color: Colors.white,
-//                         fontSize: 28,
-//                         fontWeight: FontWeight.w700,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//             Spacer(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
 
-//   @override
-//   void dispose() {
-//     stopListening();
-//     super.dispose();
-//   }
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
 
-//   Widget gradientShaderMask({@required Widget child}) {
-//     return ShaderMask(
-//       shaderCallback: (bounds) => LinearGradient(
-//         colors: [
-//           Colors.orange,
-//           Colors.deepOrange.shade900,
-//         ],
-//         begin: Alignment.topCenter,
-//         end: Alignment.bottomCenter,
-//       ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
-//       child: child,
-//     );
-//   }
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
 
-//   void startListening() {
-//     _pedometer = Pedometer();
-//     _subscription = _pedometer.pedometerStream.listen(
-//       getTodaySteps,
-//       onError: _onError,
-//       onDone: _onDone,
-//       cancelOnError: true,
-//     );
-//   }
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
 
-//   void _onDone() => print("Finished pedometer tracking");
-//   void _onError(error) => print("Flutter Pedometer Error: $error");
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
 
-//   Future<int> getTodaySteps(int value) async {
-//     print(value);
-//     int savedStepsCountKey = 999999;
-//     int savedStepsCount = stepsBox.get(savedStepsCountKey, defaultValue: 0);
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
 
-//     int todayDayNo = Jiffy(DateTime.now()).dayOfYear;
-//     if (value < savedStepsCount) {
-//       // Upon device reboot, pedometer resets. When this happens, the saved counter must be reset as well.
-//       savedStepsCount = 0;
-//       // persist this value using a package of your choice here
-//       stepsBox.put(savedStepsCountKey, savedStepsCount);
-//     }
+    if (!mounted) return;
+  }
 
-//     // load the last day saved using a package of your choice here
-//     int lastDaySavedKey = 888888;
-//     int lastDaySaved = stepsBox.get(lastDaySavedKey, defaultValue: 0);
-
-//     // When the day changes, reset the daily steps count
-//     // and Update the last day saved as the day changes.
-//     if (lastDaySaved < todayDayNo) {
-//       lastDaySaved = todayDayNo;
-//       savedStepsCount = value;
-
-//       stepsBox
-//         ..put(lastDaySavedKey, lastDaySaved)
-//         ..put(savedStepsCountKey, savedStepsCount);
-//     }
-
-//     setState(() {
-//       todaySteps = value - savedStepsCount;
-//     });
-//     stepsBox.put(todayDayNo, todaySteps);
-//     return todaySteps; // this is your daily steps value.
-//   }
-
-//   void stopListening() {
-//     _subscription.cancel();
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pedometer example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Steps taken:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: TextStyle(fontSize: 60),
+              ),
+              Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? TextStyle(fontSize: 30)
+                      : TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
